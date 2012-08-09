b948eec3d93ba9313a04d3f0b0e7016fafaf1722
Ryan Dodd <rcd1214@hotmail.com>
2012-08-09 03:24:35 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plzdontbyteme::Application.config.secret_token = '267c50c3ff08c21856a7f7a34f679ec7cb4dd4e3bd1a47ee724ad8c7154501ed2dba4f9694ff0e4c8ac8219d2c28191338ff244d08aea2b45bd830f12cdbda61'
